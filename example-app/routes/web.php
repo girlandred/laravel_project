6e68d6ff7e107aed94004ae81ac24ab3f7ff49e0
Maryna Shyta <marinka.shita@gmail.com>
2022-01-14 12:32:54 +0200

    products table and controller was created

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('products', ProductController::class);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
