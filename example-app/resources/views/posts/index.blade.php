85545ccd9177623f02d017e06fdc19254f92bd13
Maryna Shyta <marinka.shita@gmail.com>
2022-01-17 15:53:05 +0200

    styles working

--- example-app/resources/views/posts/index.blade.php
+++ example-app/resources/views/posts/index.blade.php
@@ -1,61 +1,55 @@
 @extends('layouts.layout')
 
 @section('content')
-
-    <div class="card-container">
-
-        <div class="row">
-            <div class="content col-12">
-                @if ($message = Session::get('success'))
-                    <div class="alert alert-success">
-                        <p>{{ $message }}</p>
-                    </div>
-                @endif
-                @foreach ($posts as $post)
+    <div class="row">
+        <div class="content col-12">
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+            @foreach ($posts as $post)
+                <div class="card">
                     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-                        <div>
+                        <div class="crop">
                             <img src="{{ asset('images/' . $post->image_path) }}" alt="">
                         </div>
-                        <div>
-                            <h2 class="text-gray-700 font-bold text-5xl pb-4">
-                                {{ $post->title }}
-                            </h2>
+                        <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                            {{ $post->title }}
+                        </h2>
 
-                            <span class="text-gray-500">
-                                By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on
-                                {{ date('jS M Y', strtotime($post->updated_at)) }}
+                        <span class="text-gray-500">
+                            By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on
+                            {{ date('jS M Y', strtotime($post->updated_at)) }}
+                        </span>
+
+                        <a href="{{ route('posts.show', $post->id) }}"
+                            class="text-gray-700 italic hover:text-gray-900">Show </a>
+
+                        @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                            <span class="float-right">
+                                <a href="{{ route('posts.edit', $post->id) }}"
+                                    class=" text-gray-700 italic hover:text-gray-900">
+                                    Edit
+                                </a>
                             </span>
 
-                            <a href="{{ route('posts.show', $post->id) }}"
-                                class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-                                Keep Reading
-                            </a>
+                            <span class="float-right">
+                                <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                    @csrf
+                                    @method('delete')
 
-                            @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
-                                <span class="float-right">
-                                    <a href="{{ route('posts.edit', $post->id) }}"
-                                        class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
-                                        Edit
-                                    </a>
-                                </span>
-
-                                <span class="float-right">
-                                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
-                                        @csrf
-                                        @method('delete')
-
-                                        <button class="btn btn-primary" type="submit">
-                                            Delete
-                                        </button>
-                                    </form>
-                                </span>
-                            @endif
-                        </div>
+                                    <button class="btn btn-primary" type="submit">
+                                        Delete
+                                    </button>
+                                </form>
+                            </span>
+                        @endif
                     </div>
-                @endforeach
-                {{ $posts->links() }}
-
-            </div>
+                </div>
+            @endforeach
+            {{ $posts->links() }}
         </div>
     </div>
+
 @endsection
--- example-app/resources/views/products/index.blade.php
+++ example-app/resources/views/products/index.blade.php
@@ -1,55 +1,50 @@
 @extends('layouts.layout')
 
-    @section('content')
+@section('content')
 
-        <div class="container">
-
-            <div class="row">
-                <div class="content col-12">
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
-
-                    <table class="table table-bordered">
-                        <thead>
-                            <th>#</th>
-                            <th>Title</th>
-                            <th>Vendor</th>
-                            <th>Country</th>
-                            <th>Quantity</th>
-                            <th width="280px">Action</th>
-                        </thead>
-                        <tbody>
-
-                            @foreach ($products as $key => $product)
-                                <tr>
-                                    <td>{{ $key + 1 }}</td>
-                                    <td>{{ $product->title }}</td>
-                                    <td>{{ $product->vendor }}</td>
-                                    <td>{{ $product->country }}</td>
-                                    <td>{{ $product->quantity }}</td>
-
-                                    <td>
-                                        <form action="{{ route('products.destroy', $product->id) }}" method="POST">
-                                            <a class="btn btn-primary"
-                                                href="{{ route('products.show', $product->id) }}">Show</a>
-                                            <a class="btn btn-primary"
-                                                href="{{ route('products.edit', $product->id) }}">Edit</a>
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-primary">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-
-                    </table>
-                    {{ $products->links() }}
-
+    <div class="row">
+        <div class="content col-12">
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
                 </div>
-            </div>
+            @endif
+
+            <table class="table table-bordered">
+                <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Vendor</th>
+                    <th>Country</th>
+                    <th>Quantity</th>
+                    <th width="280px">Action</th>
+                </thead>
+                <tbody>
+
+                    @foreach ($products as $key => $product)
+                        <tr>
+                            <td>{{ $key + 1 }}</td>
+                            <td>{{ $product->title }}</td>
+                            <td>{{ $product->vendor }}</td>
+                            <td>{{ $product->country }}</td>
+                            <td>{{ $product->quantity }}</td>
+
+                            <td>
+                                <form action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                    <a class="btn btn-primary" href="{{ route('products.show', $product->id) }}">Show</a>
+                                    <a class="btn btn-primary" href="{{ route('products.edit', $product->id) }}">Edit</a>
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-primary">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+
+            </table>
+            {{ $products->links() }}
+
         </div>
-    @endsection
+    </div>
+@endsection
